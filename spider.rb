a34eb610f621bff760b28254c62010f30b435c3e
Cam Crews <camcrews@gmail.com>
2017-03-14 17:46:15 -0400

    Walk-through with selectors
    README.md includes details about CSS and XPath selectors
    reverted custom Hash generation in favor of sqlite3 results_as_hash flag
    added batch calculation

--- spider.rb
+++ spider.rb
@@ -1,31 +1,31 @@
 # spider.rb
-
+# This spider has been set to process data
+# from the Princeton review.  We've preloaded
+# two schools and will run a batch to capture
+# data about each school
 require 'open-uri'
 require 'nokogiri'
 require 'sqlite3'
 
 # rather than use an ORM like ActiveRecord, 
-# we can just use sqlite3 directly:
+# we can just use sqlite3 directly
+# ref: http://www.rubydoc.info/github/luislavena/sqlite3-ruby/SQLite3/Database
+
 # open connection to database
 $db = SQLite3::Database.new "db/scraper.sqlite3"
+# we'd prefer if results are returned as a hash
+$db.results_as_hash = true
 
-# by default, sqlite3 will return results as an array
-# rather than a hash, so we will want to keep track 
-# of column positions...
-$col_school = %w(id name url_source url_school)
-$col_school_capture = %w(school_id batch applicants acceptance_rate sat_reading_lo sat_reading_hi sat_math_lo sat_math_hi sat_writing_lo sat_writing_hi act_lo act_hi selectivity_rating deadline_early deadline_late)
-
-# this is another reason why using an ORM can be helpful
-# as it would allow us to avoid managing this ourselves
+# each time we run our script, we'll refer to 
+# this as a "batch." So, we'll want to figure out
+# what the current batch number should be by
+# looking into our database and finding the 
+# largest batch that has previously been recorded
+$batch = ($db.get_first_value("SELECT MAX(batch) FROM school_capture;") || 0) + 1
 
 # now let's retrieve schools for our scraper
 # to get data on
-$schools = $db.query("SELECT * FROM school").map do |row|
-    row.each_with_index.reduce({}) do |hash,(value,index)|
-      hash[$col_school[index]] = value
-      hash
-    end
-end
+$schools = $db.query("SELECT * FROM school;")
 
 # We now will want to iterate over each of the schools
 # in our list and pull data from the web about them
@@ -38,4 +38,8 @@ $schools.each do |school|
 end
 
 # to do: process the documents we've collected!
+# ref: http://www.nokogiri.org/tutorials/searching_a_xml_html_document.html
+$docs.each do |doc|
+  
+end
 
